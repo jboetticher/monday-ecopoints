9e3d482dbdfc3c66c3607cf508808d43466e3121
Jeremy Boetticher <jb@Jeremys-MacBook-Pro.local>
2022-06-06 21:56:09 -0700

    Added eco points calculation

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ const App = props => {
 
   const [boardCxt, setBoardCxt] = useState(null);
   const [boardData, setBoardData] = useState(null);
+  const [settings, setSettings] = useState(null);
 
   useEffect(() => {
     monday.listen("context", res => {
       setBoardCxt(res.data);
-      console.log(res.data);
       
-      monday.api(`query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1) { name column_values { title text } } } }`,
+      monday.api(`query ($boardIds: [Int]) { boards (ids:$boardIds) { name items(limit:1000) { name column_values { title text } } } }`,
         { variables: {boardIds: res.data.boardIds} }
       )
       .then(res => {
@@ -25,15 +25,39 @@ const App = props => {
         setBoardData(res.data);
       });
     });
+    
+    monday.listen("settings", res => {
+      setSettings(res.data);
+      console.log(res.data);
+    });
+
   }, []);
 
-  
+  // Calculate total eco points
+  let ecoPoints = 0;
+  let ecoPointColumn = 'EcoPoints';
+  if(settings != null && settings.ecopointcolumn != null && settings.ecopointcolumn !== '') 
+    ecoPointColumn = settings.ecopointcolumn;
+  console.log(ecoPointColumn);
+  if(boardData) {
+    for(let b of boardData.boards) {
+      for(let item of b.items) {
+        for(let column of item.column_values) {
+          if(column.title === ecoPointColumn) {
+            const points = parseInt(column.text);
+            console.log('adding eco points for ' + item.name, points);
+            if(!isNaN(points)) ecoPoints += points;
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div className="App">
       <AttentionBox
         title="Carbon Cruncher"
-        text={boardData?.boards[0].name}
+        text={'Total Eco Points: ' + ecoPoints}
         type="success"
       />
     </div>
