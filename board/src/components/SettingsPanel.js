84c781ee655031c252870fd9ebd090dcf9c5a9c1
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-07-03 17:50:44 -0700

    Inported stripe + styling

--- board/src/components/SettingsPanel.js
+++ board/src/components/SettingsPanel.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "monday-ui-react-core/dist/main.css"
 //Explore more Monday React Components here: https://style.monday.com/
-import Flex from "monday-ui-react-core/dist/Flex";
-import Avatar from "monday-ui-react-core/dist/Avatar";
-import Divider from "monday-ui-react-core/dist/Divider";
+import Button from "monday-ui-react-core/dist/Button";
 import Heading from "monday-ui-react-core/dist/Heading";
 import Slider from "monday-ui-react-core/dist/Slider";
 import Accordion from "monday-ui-react-core/dist/Accordion";
@@ -14,38 +12,56 @@ import AccordionItem from "monday-ui-react-core/dist/AccordionItem";
  */
 const SettingsPanel = props => {
 
+  const [carbonPledge, setCarbonPledge] = useState(5);
+
   const logoContainerStyle = {
     display: 'flex',
     justifyContent: 'center',
     width: '100%'
   }
 
+  function removeCarbonRequest(e) {
+    alert("OOGLY");
+    fetch("https://us-central1-monday-ecopoints.cloudfunctions.net/authorizationTest")
+      .then(res => console.log(res));
+  }
+
   return (
     <div className="whitepanel">
       <div style={logoContainerStyle}>
         <img
-          src="https://cdn.discordapp.com/attachments/426940183112318976/985436645962686504/EcoPointx128.png"
-          style={{ margin: 'auto' }}
+          src="https://cdn.discordapp.com/attachments/757328909686669362/992601138274762762/EcoPoints_Text.png"
+          style={{ margin: 'auto', height: "128px", marginBottom: '1rem' }}
         />
       </div>
-
-      <p>EcoPoints lets teams save the world one task at a time.</p>
-
 
       <Accordion className="monday-storybook-accordion_small-wrapepr" defaultIndex={[0]}>
         <AccordionItem title="Remove Carbon">
           <div className="abox">
-            <div>How many tons of carbon do you want to remove?</div>
-            <Heading value="Monthly tons pledged" customColor={'black'} type={Heading.types.h4} />
-            <Slider />
+            <div style={{ marginBottom: '2.5rem' }}>How many tons of carbon do you want to remove?</div>
+            <Slider
+              step={1} min={1} max={10} defaultValue={5} color={Slider.colors.POSITIVE}
+              showValue={true} valueFormatter={value => `${value}t`}
+              onChange={x => setCarbonPledge(x)}
+            />
+            <div style={{ marginTop: '1rem' }} />
+            <div style={{ display: "flex" }}>
+              <div style={{ fontSize: '36px', width: '40%', color: 'var(--color-success)' }}>${carbonPledge * 5}.00</div>
+              <div style={{ width: '60%', textAlign: 'right' }}>
+                <Button color={Button.colors.POSITIVE} onClick={removeCarbonRequest}>Remove Carbon!</Button>
+              </div>
+            </div>
+            <div style={{ marginTop: '1rem' }}>
+              Last pledge fulfilled: 12/12/12
+            </div>
           </div>
         </AccordionItem>
         <AccordionItem title="How it Works">
           <div className="abox">
             <Heading value="EcoPoints for teams" customColor={'black'} type={Heading.types.h4} />
             <div>
-              EcoPoints can be anything. Sales, time spent, or just an arbitrary number. As long 
-              as it's a number column in your tasks, you can set it in your settings!
+              EcoPoints can be anything and are calculated <b>per board</b>. Sales, time spent, or just an 
+              arbitrary number. As long as it's a number column in your tasks, you can set it in your settings!
             </div>
             <div>
               Teams pledge to remove carbon in exchange for productivity, and Team Members that
@@ -60,7 +76,7 @@ const SettingsPanel = props => {
               carbon dioxide through natural processes, like planting trees.
             </div>
             <div>
-              Learn more about carbon offsets 
+              Learn more about carbon offsets
               <a href="https://www.southpole.com/carbon-offsets-explained" target="_blank"> here</a>.
             </div>
           </div>
@@ -78,8 +94,11 @@ const SettingsPanel = props => {
           </div>
         </AccordionItem>
         <AccordionItem title="Troubleshooting">
-          <div>
-
+          <div className="abox">
+            <div>
+              If your EcoPoints are not loading, be sure to check the settings. The <b>EcoPoint Column</b> must be a number
+              column. The <b>Task Assignee Column</b> must be a people column.
+            </div>
           </div>
         </AccordionItem>
         <AccordionItem title="Donation Type">
--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,12 +1,23 @@
 import * as functions from "firebase-functions";
-import {testPrivateKey, prodPrivateKey, rpcProvider} from "./secrets";
+import {testPrivateKey, prodPrivateKey, rpcProvider, stripeKey} from "./secrets";
 import {OffsetHelperABI} from "./abi";
 import {ethers} from "ethers";
+import * as fb from "firebase-admin";
+import Stripe from "stripe";
 
 const isProduction = true;// process.env.NODE_ENV === "production";
+
+// Ethers contract creation
 const provider = new ethers.providers.JsonRpcProvider(rpcProvider);
 const signer = new ethers.Wallet(isProduction ? prodPrivateKey : testPrivateKey, provider);
 const offsetHelperContract = new ethers.Contract(contract("OffsetHelper"), OffsetHelperABI, signer);
+
+// Stripe payment initialization
+const stripe = new Stripe(stripeKey, { apiVersion: '2020-08-27' });
+
+// Facebook app initialization
+fb.initializeApp();
+const db = fb.firestore();
 
 // Returns an address based on whether or not in production
 function contract(symbol: "USDC" | "NCT" | "OffsetHelper", forceProduction = false) : string {
@@ -48,19 +59,51 @@ export const getCarbonCreditPrice = functions.https.onRequest(async (request, re
 });
 
 export const burnTokens = functions.https.onRequest(async (request, response) => {
-    const amount = parseInt(request.body.amount);
-    if(isNaN(amount) || amount < 0 || amount > 10) {
-        response.status(400).json({ success: false, reason: "Invalid amount." });
-        return;
-    }
+  const amount = parseInt(request.body.amount);
+  if (isNaN(amount) || amount < 0 || amount > 10) {
+    response.status(400).json({success: false, reason: "Invalid amount."});
+    return;
+  }
 
-    // Redeem NCT for carbon CAPTURE credit & redeem the carbon capture credit
-    // We're using the offset helper contract so that we don't need 2 transactions
-    const ethResponse = await offsetHelperContract.autoOffsetUsingToken(
-        contract('USDC'), 
-        contract('NCT'), 
-        amount + "000000000000000000"
-    );
+  // Redeem NCT for carbon CAPTURE credit & redeem the carbon capture credit
+  // We're using the offset helper contract so that we don't need 2 transactions
+  const ethResponse = await offsetHelperContract.autoOffsetUsingToken(
+      contract("USDC"),
+      contract("NCT"),
+      amount + "000000000000000000"
+  );
 
-    response.status(200).json({ success: true, ethResponse });
+  response.status(200).json({success: true, ethResponse});
+});
+
+export const lastPurchase = functions.https.onRequest(async (request, response) => {
+  const board = Number.parseInt(request.body.board);
+
+  console.log(typeof(board), board);
+  if (typeof(board) !== "number" || isNaN(board)) {
+    response.status(400).json({success: false, reason: "Board incorrect"});
+    return;
+  }
+
+  const res = await db.collection("purchases").where("board", "==", board).orderBy("date", "desc").limit(1).get();
+  response.status(200).json({success: true, ...(res.docs[0].data())});
+});
+
+export const createCheckoutSession = functions.https.onRequest(async (request, response) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: '{{PRICE_ID}}',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+    automatic_tax: {enabled: true},
   });
+
+  res.redirect(303, session.url);
+});
+});
